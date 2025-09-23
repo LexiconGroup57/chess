682fb55b5a9c8f80dddea0de538288cb07baf046
Tobias Engberg <tobias.engberg@exilic.se>
2025-09-23 10:45:20 +0200

    Move player

--- json.js
+++ json.js
@@ -10,3 +10,34 @@ let newJSON = {
         "cities": ["Flen", null],
     }
 }
+
+// map
+// spread
+// destructuring
+
+let testArray = ["Dog", "Cat", "Bird", "Caterpillar"];
+
+let target = document.getElementById("board");
+testArray.map(item => {
+    let newElement = document.createElement("p");
+    target.appendChild(newElement);
+    newElement.textContent = item;
+});
+
+const keepPets = (pet1, pet2, pet3, pet4) => {
+    console.log(pet1 + " " + pet2);
+}
+
+let copyArray = [...testArray];
+copyArray[0] = "Dragon";
+
+keepPets(...testArray);
+
+let leftSide = "RightSide";
+let [pet1, pet2, pet3, pet4] = testArray;
+
+const returnTwo = () => {
+    return ["Bob", () => {}];
+}
+
+let [first, second] = returnTwo();
--- script.js
+++ script.js
@@ -10,16 +10,33 @@ let pieces = [
     ['w1', 'w2', 'w3', 'w4', 'w5', 'w3', 'w2', 'w1'],
 ]
 
-let board = document.getElementById("board");
-for (let k = 0; k < 8; k++){
-    for (let m = 0; m < 8; m++){
-        let newElement = document.createElement("div");
+const regenerateBoard = () => {
+    let board = document.getElementById("board");
+    board.innerHTML = '';
+    for (let k = 0; k < 8; k++){
+        for (let m = 0; m < 8; m++){
+            let newElement = document.createElement("div");
 
-        k % 2 === m % 2 ? newElement.classList.add("white", pieces[k][m]) : newElement.classList.add("black", pieces[k][m]);
-        board.appendChild(newElement);
+            k % 2 === m % 2 ? newElement.classList.add("white", pieces[k][m]) : newElement.classList.add("black", pieces[k][m]);
+            board.appendChild(newElement);
+        }
     }
-
 }
 
+const movePlayer = (positions) => {
+    console.log(positions);
+    pieces[positions.newRow][positions.newColumn] = pieces[positions.oldRow][positions.oldColumn];
+    pieces[positions.oldRow][positions.oldColumn] = 'n0';
+}
 
+document.getElementById("start").addEventListener('click', (e) => {
+    regenerateBoard();
+})
 
+document.getElementById("move").addEventListener('submit', (e) => {
+    e.preventDefault();
+    let user =document.getElementById("positions").value;
+    let positions = { 'oldRow': Number(user[0]), 'oldColumn': Number(user[1]), 'newRow': Number(user[2]), 'newColumn': Number(user[3]) };
+    movePlayer(positions);
+    regenerateBoard();
+})
