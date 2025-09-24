3512e385507fce38b7e97087457fd73e3a2ec614
tobiasengberg <tobias.engberg@exilic.se>
2025-09-24 10:34:47 +0200

    Storage and add check move

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+let turn = 'w';
+
 let pieces = [
     ['b1', 'b2', 'b3', 'b4', 'b5', 'b3', 'b2', 'b1'],
     ['b6', 'b6', 'b6', 'b6', 'b6', 'b6', 'b6', 'b6'],
@@ -23,10 +25,44 @@ const regenerateBoard = () => {
     }
 }
 
-const movePlayer = (positions) => {
-    console.log(positions);
-    pieces[positions.newRow][positions.newColumn] = pieces[positions.oldRow][positions.oldColumn];
-    pieces[positions.oldRow][positions.oldColumn] = 'n0';
+const checkPossibleMove = (move) => {
+    if( move.oldRow === move.newRow && move.oldColumn === move.newColumn) return false;
+    if (pieces[move.oldRow][move.oldColumn][0] !== turn) return false;
+
+    let black = pieces[move.oldRow][move.oldColumn][0] === 'b';
+    let white = pieces[move.oldRow][move.oldColumn][0] === 'w';
+    let rowOne = move.oldRow === 1;
+    let rowSix = move.oldRow === 6;
+    let rowPlusTwo = move.newRow === 3;
+    let rowMinusTwo = move.newRow === 4;
+    let sameColumn = move.oldColumn === move.newColumn;
+    let emptyTarget = pieces[move.newRow][move.newColumn] === 'n0';
+
+    const determineClearLine = () => {
+        return true;
+    }
+    switch(pieces[move.oldRow][move.oldColumn][1]){
+        case '1':       // Rook
+            return true;
+        case '2':       // Knight
+            return true;
+        case '3':       // Bishop
+            return true;
+        case '4':       // Queen
+            return true;
+        case '5':       // King
+            return true;
+        case '6':       // Pawn
+            if(((black && rowOne && rowPlusTwo) || (white && rowSix && rowMinusTwo)) && sameColumn && emptyTarget && determineClearLine()) return true;
+            return false;
+        default:
+            return false;
+    }
+}
+
+const movePlayer = (move) => {
+    pieces[move.newRow][move.newColumn] = pieces[move.oldRow][move.oldColumn];
+    pieces[move.oldRow][move.oldColumn] = 'n0';
 }
 
 document.getElementById("start").addEventListener('click', (e) => {
@@ -37,6 +73,10 @@ document.getElementById("move").addEventListener('submit', (e) => {
     e.preventDefault();
     let user =document.getElementById("positions").value;
     let positions = { 'oldRow': Number(user[0]), 'oldColumn': Number(user[1]), 'newRow': Number(user[2]), 'newColumn': Number(user[3]) };
-    movePlayer(positions);
-    regenerateBoard();
+    if (checkPossibleMove(positions)) {
+        movePlayer(positions);
+        regenerateBoard();
+        turn = turn === 'w' ? 'b' : 'w';
+    }
+
 })
---
+++ storage.js
@@ -0,0 +1,11 @@
+
+
+let element = document.getElementById('positions');
+
+if(sessionStorage.getItem('positions') !== null)
+    element.value = sessionStorage.getItem('positions')
+
+element.addEventListener('change', () => {
+    console.log(element.value);
+    sessionStorage.setItem('positions',  element.value);
+})
